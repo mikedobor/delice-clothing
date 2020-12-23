283b0727bf08855f4fe35c72bd185b4395711615
Michael Aigboeghian <micki1.com@gmail.com>
2020-12-23 23:22:55 +0100

    Implemented custom button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.styles.scss';
 
@@ -35,7 +36,7 @@ export default class SignIn extends Component {
                     <FormInput type='email' name='email' value={this.state.email} handleChange={this.handleChange} label='Email' required />
                     <FormInput type='password' name='password' value={this.state.password} handleChange={this.handleChange} label='P assword' required />
 
-                    <input type="submit" value="Submit Form"/>
+                    <CustomButton type="submit" >Sign in</CustomButton>
                 </form>
             </div>
         );
