07475da985a0b89b951e77043e5b5b904b613b5f
Michael Aigboeghian <micki1.com@gmail.com>
2020-12-24 13:56:09 +0100

    Implemented sign up with email and password feature

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -35,7 +35,7 @@ export default class SignIn extends Component {
 
                 <form onSubmit={this.handleSubmit}>
                     <FormInput type='email' name='email' value={this.state.email} handleChange={this.handleChange} label='Email' required />
-                    <FormInput type='password' name='password' value={this.state.password} handleChange={this.handleChange} label='P assword' required />
+                    <FormInput type='password' name='password' value={this.state.password} handleChange={this.handleChange} label='Password' required />
                     <div className='buttons'>
                         <CustomButton type="submit" >Sign in</CustomButton>
                         <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
