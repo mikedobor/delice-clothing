7c9c04baf9b3c03d57bb08fc84c858a093c9f330
Michael Aigboeghian <micki1.com@gmail.com>
2020-12-25 11:42:07 +0100

    created memoized selector for display number of items in cart in cart icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,17 +6,19 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden, itemsCount }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
          <ShoppingIcon className='shopping-icon' />
-         <span className='item-count'>{itemsCount}</span>
+         <span className='item-count'>{itemCount}</span>
     </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-    itemsCount: cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
 })
 
 const mapDispatchToProps = dispatch => ({
