57e52fc811403c4573d6ed79828a19b2c045e0c5
Michael Aigboeghian <micki1.com@gmail.com>
2021-01-05 18:11:32 +0100

    Implemented sagas for clearing cart on user sign out

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,5 +1,5 @@
 import "regenerator-runtime";
-import { takeLatest, call, put } from "redux-saga/effects";
+import { takeLatest, call, put, all } from "redux-saga/effects";
 import {
   firestore,
   convertCollectionsSnapshotToMap,
@@ -27,4 +27,8 @@ export function* fetchCollections() {
 
 export function* onFetchCollectionsStart() {
   yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollections);
+}
+
+export function* shopSagas() {
+  yield all([call(onFetchCollectionsStart)]);
 }
